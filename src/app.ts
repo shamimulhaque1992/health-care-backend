275dea714322d23d6f8f73b13f0f136cf0ec3df7
shamimulhaquewebsure <shamimul@websure.digital>
2026-08-17 12:44:21 +0600

    otp set in redis is done.

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, {
-	type Application,
-	type Request,
-	type Response,
+  type Application,
+  type Request,
+  type Response,
 } from "express";
 import httpStatus from "http-status";
 import config from "./app/config";
 import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import { notFound } from "./app/middleware/notFound";
 import { AuthRoutes } from "./app/module/auth/auth.route";
+import { redisClient } from "./app/lib/redis";
 
 const app: Application = express();
 
 app.use(
-	cors({
-		origin: config.frontend_url,
-		credentials: true,
-	}),
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  }),
 );
 
 // Enable URL-encoded form data parsing
@@ -29,12 +30,28 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", AuthRoutes);
 
+app.get("/redis", async (res: Response) => {
+  try {
+    await redisClient.set("forgot-password-otp:patient1@gmail.com", "123456", {
+      expiration: {
+        type: "EX",
+        value: 60,
+      },
+    });
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "OTP set successfully in Redis",
+    });
+  } catch (error) {}
+});
+
 // Basic route
 app.get("/", async (req: Request, res: Response) => {
-	res.status(httpStatus.OK).json({
-		success: true,
-		message: "Welcome to PH Healthcare System Backend",
-	});
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to PH Healthcare System Backend",
+  });
 });
 
 app.use(globalErrorHandler);
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -15,7 +15,7 @@ export default {
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
   google_client_id: process.env.GOOGLE_CLIENT_ID!,
-  
+
   super_admin_name: process.env.SUPER_ADMIN_NAME!,
   super_admin_email: process.env.SUPER_ADMIN_EMAIL!,
   super_admin_password: process.env.SUPER_ADMIN_PASSWORD!,
@@ -27,4 +27,9 @@ export default {
   tester_doctor_name: process.env.TESTER_DOCTOR_NAME!,
   tester_doctor_email: process.env.TESTER_DOCTOR_EMAIL!,
   tester_doctor_password: process.env.TESTER_DOCTOR_PASSWORD!,
+
+  redis_user: process.env.REDIS_USER!,
+  redis_password: process.env.REDIS_PASSWORD!,
+  redis_host: process.env.REDIS_HOST!,
+  redis_port: process.env.REDIS_PORT!,
 };
